be374499ef7a09865f107bc676c8ffdace1c9e2b
morgacoo <morgacoo@gmail.com>
2017-08-29 10:57:40 -0500

    started adding js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-function flash(e){
-  $('.flash')
+/*function flash(e){
+  $('#header_logo')
    .show()  //show the hidden div
    .animate({opacity: 0.5}, 300)
    .fadeOut(300)
-   .css({'opacity': 1});
+   .css({'opacity': 2});
 }
 
 $(document).ready(function() {
-  $('.flash').hide();
+  $('#header_logo').hide();
   $(document).mouseup(function(e) { flash(e); });
-});
+});*/
